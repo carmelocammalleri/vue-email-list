0099e02af87a527c1cf881d165dd7fbad2dde2f6
Melozzo <carmecamma1995@gmail.com>
2023-10-06 16:34:51 +0200

    [add]: comments

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
+
+      //Array vuoto per accogliere gli elementi che si creano con il ciclo
       emails: [],
       apriUrl: "https://flynn.boolean.careers/exercises/api/random/mail?min=1&max=10"
     }
@@ -10,9 +12,14 @@ createApp({
 
   methods:{
     getApi(){
+      // ciclo che generi 10 emails
       for(let i=0; i<10; i++){
+
+        // creazione singolo elemento randomico
         axios.get(this.apriUrl)
         .then((risposta)=>{
+
+          //pusho il singolo elemento all'interno dell'array principale "emails"
           this.emails.push(risposta.data.response)
           console.log(risposta.data.response);
       })
@@ -21,6 +28,7 @@ createApp({
   },
 
   mounted(){
+    // richiama la funzione e la monta sull'HTML
     this.getApi()
     console.log(this.emails);
   }
